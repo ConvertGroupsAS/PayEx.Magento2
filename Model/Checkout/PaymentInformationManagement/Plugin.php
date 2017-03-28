c655975d3f3b8f98b259b3f48067c87193c85c1b
Volodymyr Malyk <vladimir@convert.no>
2017-03-28 13:04:40 +0300

    [EPLE-401] removed around plugin for SavePaymentInformationAndPlaceOrder

--- Model/Checkout/PaymentInformationManagement/Plugin.php
+++ Model/Checkout/PaymentInformationManagement/Plugin.php
@@ -50,39 +50,4 @@ class Plugin
             $this->session->setPayexSSN(isset($additionalData['social_security_number']) ? $additionalData['social_security_number'] : null);
         }
     }
-
-    /**
-     * Set payment information and place order for a specified cart.
-     *
-     * Override this method to get correct exceptions instead
-     * "An error occurred on the server. Please try to place the order again."
-     *
-     * @param \Magento\Checkout\Model\PaymentInformationManagement $subject
-     * @param \Closure $proceed
-     * @param $cartId
-     * @param \Magento\Quote\Api\Data\PaymentInterface $paymentMethod
-     * @param \Magento\Quote\Api\Data\AddressInterface|null $billingAddress
-     * @throws \Magento\Framework\Exception\CouldNotSaveException
-     * @return int Order ID.
-     */
-    public function aroundSavePaymentInformationAndPlaceOrder(
-        \Magento\Checkout\Model\PaymentInformationManagement $subject,
-        \Closure $proceed,
-        $cartId,
-        \Magento\Quote\Api\Data\PaymentInterface $paymentMethod,
-        \Magento\Quote\Api\Data\AddressInterface $billingAddress
-    )
-    {
-        $subject->savePaymentInformation($cartId, $paymentMethod, $billingAddress);
-        try {
-            $orderId = $this->cartManagement->placeOrder($cartId);
-        } catch (\Exception $e) {
-            throw new CouldNotSaveException(
-                __($e->getMessage()),
-                $e
-            );
-        }
-
-        return $orderId;
-    }
 }
