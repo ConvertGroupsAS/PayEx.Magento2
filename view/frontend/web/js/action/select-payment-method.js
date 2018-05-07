69d136d14a639831d251fc0c6120b1e90a566183
Andrej Terentev <andrej.te@convert.no>
2018-05-07 12:47:55 +0300

    [EPLECARE-271] Fix nullable paymentMethod on checkout

--- view/frontend/web/js/action/select-payment-method.js
+++ view/frontend/web/js/action/select-payment-method.js
@@ -12,20 +12,22 @@ define(
     function (urlBuilder, storage, quote, fullScreenLoader, jQuery, getTotalsAction) {
         'use strict';
         return function (paymentMethod) {
-            quote.paymentMethod(paymentMethod);
+            if (paymentMethod) {
+                quote.paymentMethod(paymentMethod);
 
-            var serviceUrl = urlBuilder.createUrl('/payex/payments/apply_payment_method', {});
-            fullScreenLoader.startLoader();
-            return storage.post(
-                serviceUrl,
-                JSON.stringify({payment_method: paymentMethod.method}),
-                true
-            ).always(function () {
-                fullScreenLoader.stopLoader();
-            }).complete(function () {
-                getTotalsAction([]);
-                fullScreenLoader.stopLoader();
-            });
+                var serviceUrl = urlBuilder.createUrl('/payex/payments/apply_payment_method', {});
+                fullScreenLoader.startLoader();
+                return storage.post(
+                    serviceUrl,
+                    JSON.stringify({payment_method: paymentMethod.method}),
+                    true
+                ).always(function () {
+                    fullScreenLoader.stopLoader();
+                }).complete(function () {
+                    getTotalsAction([]);
+                    fullScreenLoader.stopLoader();
+                });
+            }
         }
     }
 );
