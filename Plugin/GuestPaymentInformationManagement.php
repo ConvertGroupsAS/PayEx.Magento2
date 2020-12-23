b4c9003409f69d9a27919eb7bc1e7d407c90c703
Andrej Terentev <andrej.te@convert.no>
2020-12-23 18:17:51 +0300

    [EPLECARE-1282] Remove bad plugins for checkout

--- Plugin/GuestPaymentInformationManagement.php
+++ Plugin/GuestPaymentInformationManagement.php
@@ -12,37 +12,11 @@ class GuestPaymentInformationManagement
     private $checkoutHelper;
 
     /**
-     * @var \Magento\Quote\Api\GuestCartManagementInterface
-     */
-    private $cartManagement;
-
-    /**
-     * @var \Psr\Log\LoggerInterface
-     */
-    private $logger;
-
-    /**
-     * @var \PayEx\Payments\Helper\Data
-     */
-    private $payexHelper;
-
-    /**
      * @param \Magento\Checkout\Helper\Data $checkoutHelper
-     * @param \Magento\Quote\Api\GuestCartManagementInterface $cartManagement
-     * @param \Psr\Log\LoggerInterface $logger
-     * @param \PayEx\Payments\Helper\Data $payexHelper
      */
-    public function __construct(
-        \Magento\Checkout\Helper\Data $checkoutHelper,
-        \Magento\Quote\Api\GuestCartManagementInterface $cartManagement,
-        \Psr\Log\LoggerInterface $logger,
-        \PayEx\Payments\Helper\Data $payexHelper
-    ) {
+    public function __construct(\Magento\Checkout\Helper\Data $checkoutHelper) {
 
         $this->checkoutHelper = $checkoutHelper;
-        $this->cartManagement = $cartManagement;
-        $this->logger = $logger;
-        $this->payexHelper = $payexHelper;
     }
 
     /**
@@ -69,56 +43,5 @@ class GuestPaymentInformationManagement
                 isset($additionalData['social_security_number']) ? $additionalData['social_security_number'] : null
             );
         }
-    }
-
-    /**
-     * Set payment information and place order for a specified cart.
-     *
-     * Override this method to get correct exceptions instead
-     * "An error occurred on the server. Please try to place the order again."
-     *
-     * @param \Magento\Checkout\Model\GuestPaymentInformationManagement $subject
-     * @param \Closure $proceed
-     * @param string $cartId
-     * @param string $email
-     * @param \Magento\Quote\Api\Data\PaymentInterface $paymentMethod
-     * @param \Magento\Quote\Api\Data\AddressInterface|null $billingAddress
-     * @throws \Magento\Framework\Exception\CouldNotSaveException
-     * @return int Order ID.
-     */
-    public function aroundSavePaymentInformationAndPlaceOrder(
-        \Magento\Checkout\Model\GuestPaymentInformationManagement $subject,
-        \Closure $proceed,
-        $cartId,
-        $email,
-        \Magento\Quote\Api\Data\PaymentInterface $paymentMethod,
-        \Magento\Quote\Api\Data\AddressInterface $billingAddress = null
-    ) {
-
-        /** @see \Magento\Checkout\Model\GuestPaymentInformationManagement::savePaymentInformationAndPlaceOrder() */
-        $subject->savePaymentInformation($cartId, $email, $paymentMethod, $billingAddress);
-        try {
-            $orderId = $this->cartManagement->placeOrder($cartId);
-        } catch (\Magento\Framework\Exception\LocalizedException $e) {
-            throw new CouldNotSaveException(
-                __($e->getMessage()),
-                $e
-            );
-        } catch (\Exception $e) {
-            if (version_compare($this->payexHelper->getMageVersion(), '2.3', '<')) {
-                throw new CouldNotSaveException(
-                    __($e->getMessage()),
-                    $e
-                );
-            } else {
-                $this->logger->critical($e);
-                throw new CouldNotSaveException(
-                    __('A server error stopped your order from being placed. Please try to place your order again.'),
-                    $e
-                );
-            }
-        }
-
-        return $orderId;
     }
 }
--- Plugin/PaymentInformationManagement.php
+++ Plugin/PaymentInformationManagement.php
@@ -12,37 +12,14 @@ class PaymentInformationManagement
     private $checkoutHelper;
 
     /**
-     * @var \Magento\Quote\Api\CartManagementInterface
-     */
-    private $cartManagement;
-
-    /**
-     * @var \Psr\Log\LoggerInterface
-     */
-    private $logger;
-
-    /**
-     * @var \PayEx\Payments\Helper\Data
-     */
-    private $payexHelper;
-
-    /**
      * @param \Magento\Checkout\Helper\Data $checkoutHelper
      * @param \Magento\Quote\Api\CartManagementInterface $cartManagement
      * @param \Psr\Log\LoggerInterface $logger
      * @param \PayEx\Payments\Helper\Data $payexHelper
      */
-    public function __construct(
-        \Magento\Checkout\Helper\Data $checkoutHelper,
-        \Magento\Quote\Api\CartManagementInterface $cartManagement,
-        \Psr\Log\LoggerInterface $logger,
-        \PayEx\Payments\Helper\Data $payexHelper
-    ) {
-
+    public function __construct(\Magento\Checkout\Helper\Data $checkoutHelper)
+    {
         $this->checkoutHelper = $checkoutHelper;
-        $this->cartManagement = $cartManagement;
-        $this->logger = $logger;
-        $this->payexHelper = $payexHelper;
     }
 
     /**
@@ -67,54 +44,5 @@ class PaymentInformationManagement
                 isset($additionalData['social_security_number']) ? $additionalData['social_security_number'] : null
             );
         }
-    }
-
-    /**
-     * Set payment information and place order for a specified cart.
-     *
-     * Override this method to get correct exceptions instead
-     * "An error occurred on the server. Please try to place the order again."
-     *
-     * @param \Magento\Checkout\Model\PaymentInformationManagement $subject
-     * @param \Closure $proceed
-     * @param $cartId
-     * @param \Magento\Quote\Api\Data\PaymentInterface $paymentMethod
-     * @param \Magento\Quote\Api\Data\AddressInterface|null $billingAddress
-     * @throws \Magento\Framework\Exception\CouldNotSaveException
-     * @return int Order ID.
-     */
-    public function aroundSavePaymentInformationAndPlaceOrder(
-        \Magento\Checkout\Model\PaymentInformationManagement $subject,
-        \Closure $proceed,
-        $cartId,
-        \Magento\Quote\Api\Data\PaymentInterface $paymentMethod,
-        \Magento\Quote\Api\Data\AddressInterface $billingAddress = null
-    ) {
-
-        /** @see \Magento\Checkout\Model\PaymentInformationManagement::savePaymentInformationAndPlaceOrder() */
-        $subject->savePaymentInformation($cartId, $paymentMethod, $billingAddress);
-        try {
-            $orderId = $this->cartManagement->placeOrder($cartId);
-        } catch (\Magento\Framework\Exception\LocalizedException $e) {
-            throw new CouldNotSaveException(
-                __($e->getMessage()),
-                $e
-            );
-        } catch (\Exception $e) {
-            if (version_compare($this->payexHelper->getMageVersion(), '2.3', '<')) {
-                throw new CouldNotSaveException(
-                    __($e->getMessage()),
-                    $e
-                );
-            } else {
-                $this->logger->critical($e);
-                throw new CouldNotSaveException(
-                    __('A server error stopped your order from being placed. Please try to place your order again.'),
-                    $e
-                );
-            }
-        }
-
-        return $orderId;
     }
 }
